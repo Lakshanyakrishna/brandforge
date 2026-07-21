0bfae6fd92b6e70b48be519726608da087eaf205
lakshanyakrishnae <beingshanya@gmail.com>
2026-07-21 16:26:04 +0530

    Make DB connection non-fatal in production; add Vercel config

--- src/db/db.js
+++ src/db/db.js
@@ -2,18 +2,37 @@ const mongoose = require('mongoose');
 const config = require('../config');
 const logger = require('../config/logger');
 
+let dbConnected = false;
+
+function isDbConnected() {
+    return dbConnected;
+}
+
 const connectDB = async () => {
-    try {
+    if (config.isProduction) {
+        try {
+            await mongoose.connect(config.mongoUri, {
+                connectTimeoutMS: 10000,
+                socketTimeoutMS: 45000,
+                serverSelectionTimeoutMS: 10000
+            });
+            dbConnected = true;
+            logger.info("Connected to MongoDB");
+        } catch (err) {
+            logger.error(`MongoDB connection failed (non-fatal): ${err.message}`);
+            logger.warn('Server will run without database. Set MONGO_URI to a valid MongoDB Atlas connection string.');
+        }
+    } else {
         await mongoose.connect(config.mongoUri, {
             connectTimeoutMS: 10000,
             socketTimeoutMS: 45000,
             serverSelectionTimeoutMS: 10000
         });
+        dbConnected = true;
         logger.info("Connected to MongoDB");
-    } catch (err) {
-        logger.error(`Error connecting to MongoDB: ${err.message}`);
-        process.exit(1);
     }
 }
+
+module.exports = { connectDB, isDbConnected };
 
 module.exports = connectDB;
