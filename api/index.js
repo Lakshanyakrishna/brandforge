d02740bb481b31d6070ac1edfe6c556157974865
lakshanyakrishnae <beingshanya@gmail.com>
2026-07-21 16:42:25 +0530

    Fix Vercel entrypoint with error handling, remove double build

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
-const app = require('../src/app');
-const { connectDB } = require('../src/db/db');
-
-connectDB();
-
-module.exports = app;
+try {
+  const app = require('../src/app');
+  const { connectDB } = require('../src/db/db');
+  connectDB();
+  module.exports = app;
+} catch (err) {
+  console.error('App load failed:', err.message, err.stack);
+  module.exports = (req, res) => {
+    res.status(500).json({ error: err.message, stack: process.env.VERCEL ? undefined : err.stack });
+  };
+}
--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-const config = require('./src/config');
-const app = require('./src/app');
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT:', err.message, err.stack);
+});
+process.on('unhandledRejection', (err) => {
+  console.error('UNHANDLED:', err.message, err.stack);
+});
+
+let app;
+try {
+  app = require('./src/app');
+} catch (err) {
+  console.error('APP LOAD FAILED:', err.message, err.stack);
+  app = require('express')();
+  app.all('*', (req, res) => res.status(500).json({ error: err.message }));
+}
+
 const { connectDB } = require('./src/db/db');
-const logger = require('./src/config/logger');
 
 if (!process.env.VERCEL) {
+  const config = require('./src/config');
+  const logger = require('./src/config/logger');
   const startServer = async () => {
       await connectDB();
       app.listen(config.port, () => {
