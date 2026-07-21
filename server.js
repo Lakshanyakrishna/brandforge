26ddcac450e435aa7a487201b8fc76d0179aa87c
lakshanyakrishnae <beingshanya@gmail.com>
2026-07-21 16:27:32 +0530

    Export Express app for Vercel serverless compatibility

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const config = require('./src/config');
 const app = require('./src/app');
-const connectDB = require('./src/db/db');
+const { connectDB } = require('./src/db/db');
 const logger = require('./src/config/logger');
 
-const startServer = async () => {
-    await connectDB();
-    app.listen(config.port, () => {
-        logger.info(`Server is running on port ${config.port}`);
-    });
+if (!process.env.VERCEL) {
+  const startServer = async () => {
+      await connectDB();
+      app.listen(config.port, () => {
+          logger.info(`Server is running on port ${config.port}`);
+      });
+  }
+  startServer();
+} else {
+  connectDB();
 }
 
-startServer();
+module.exports = app;
